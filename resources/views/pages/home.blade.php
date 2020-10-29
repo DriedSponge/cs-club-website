9b4eca4c8e254c55b14facd8d69333fb4bc3cef6
DriedSponge <jordan@driedsponge.net>
2020-10-29 07:26:13 -0700

    Fixed js not working

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,4 +3,9 @@
 @section('content')
 <h1>Test Content</h1>
 <h2>This is just some test content to make sure everything is working properly.</h2>
+<script>
+        $(()=>{
+            console.log("java script appears to be working")
+        })
+</script>
 @endsection

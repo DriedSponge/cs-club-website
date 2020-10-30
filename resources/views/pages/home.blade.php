50ead649c19b9432cdd3be38c697d91d4225117a
DriedSponge <jordan@driedsponge.net>
2020-10-30 09:54:56 -0700

    Added meta description to improve seo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('title','Home')
+@section('description',"A club that strives to build a community of students interested in Computer Science and coding that works on projects together. It encourages students to challenge themselves by attending hackathons, and competitive competitions like the U.S.A Computing Olympiad. No previous coding experience required!")
 @section('content')
 <h1>Test Content</h1>
 <h2>This is just some test content to make sure everything is working properly.</h2>

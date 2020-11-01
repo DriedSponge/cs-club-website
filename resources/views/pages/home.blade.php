543e767642fb18702bdc266f6912d3328340a066
DriedSponge <jordan@driedsponge.net>
2020-11-01 15:53:44 -0800

    Added nav bar

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,8 @@
 @section('title','Home')
 @section('description',"A club that strives to build a community of students interested in Computer Science and coding that works on projects together. It encourages students to challenge themselves by attending hackathons, and competitive competitions like the U.S.A Computing Olympiad. No previous coding experience required!")
 @section('content')
-<h1>Test Content</h1>
-<h2>This is just some test content to make sure everything is working properly.</h2>
+    <div class="container">
+        <h1 class="text-center">Welcome!</h1>
+        <h3 class="text-center">This is just some test content to make sure everything is working properly.</h3>
+    </div>
 @endsection

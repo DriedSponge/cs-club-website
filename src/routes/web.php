91484b152a056f01930836b862efd215a4cc881c
DriedSponge <jordan@driedsponge.net>
2020-11-08 02:41:55 -0800

    Removed useless route

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 })->name("home");
-
-Route::get('/test', function () {
-    return view('pages.test');
-})->name("home");

3fb58d6b8dab630f82bfada5b0c950813bfd6796
DriedSponge <jordan@driedsponge.net>
2020-11-22 12:08:59 -0800

    Fixed navbar and routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,9 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 })->name("home");
+
 Route::get('/current-projects', function () {
     return view('pages.projects');
 })->name("projects");
+
 Route::get('/events', function () {
 	return view('pages.events');
 })->name("events");

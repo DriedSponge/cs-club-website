48d830aa389b2dc9ca6e0ea5c5de2ea908028707
DriedSponge <jordan@driedsponge.net>
2020-11-22 12:03:23 -0800

    Added routes back in

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 })->name("home");
+Route::get('/current-projects', function () {
+    return view('pages.projects');
+})->name("projects");
+Route::get('/events', function () {
+	return view('pages.events');
+})->name("events");

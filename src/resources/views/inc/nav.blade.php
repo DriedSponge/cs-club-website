bd455e04112f9f454a4f4e1e83f839cc19c0c663
DriedSponge <jordan@driedsponge.net>
2020-11-25 14:44:26 -0800

    Nav links change based on what page you're on

--- src/resources/views/inc/nav.blade.php
+++ src/resources/views/inc/nav.blade.php
@@ -7,19 +7,19 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                    <a class="nav-link {{ Route::currentRouteNamed('home') ? 'active' : '' }}" href="{{route('home')}}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('members')}}">Members</a>
+                    <a class="nav-link {{ Route::currentRouteNamed('members') ? 'active' : '' }}" href="{{route('members')}}">Members</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('events')}}">Events</a>
+                    <a class="nav-link {{ Route::currentRouteNamed('events') ? 'active' : '' }}" href="{{route('events')}}">Events</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('projects')}}">Projects</a>
+                    <a class="nav-link {{ Route::currentRouteNamed('projects') ? 'active' : '' }}" href="{{route('projects')}}">Projects</a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('contactus')}}" class="nav-link" href="#">Contact Us</a>
+                    <a class="nav-link {{ Route::currentRouteNamed('contactus') ? 'active' : '' }}" href="{{route('contactus')}}" >Contact Us</a>
                 </li>
             </ul>
         </div>

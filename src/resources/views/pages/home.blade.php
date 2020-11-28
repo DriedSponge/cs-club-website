0b0d3c3fd42d06e9ee9f5f719818b81df525052e
DriedSponge <jordan@driedsponge.net>
2020-11-28 14:16:48 -0800

    Added timestamps to commits on home page and commits now update every five minutes

--- src/resources/views/pages/home.blade.php
+++ src/resources/views/pages/home.blade.php
@@ -50,7 +50,7 @@ so now we want to specify what goes in that yield for this page.
         <div class="card shadow border-0 p-3">
             <div class="card-body">
                 <h2>Recent Commits</h2>
-                <br>
+                <h6 class="text-muted fst-italic mb-4">Updated: {{\Carbon\Carbon::parse($lastupdated)->diffForHumans()}}</p></h6>
                 @foreach($commits as $commit)
                     @if($commit['author'] == null)
                         @continue
@@ -62,7 +62,7 @@ so now we want to specify what goes in that yield for this page.
                                     <img data-src="{{$commit['author']['avatar_url']}}" class="lozad image-fluid rounded-circle" width="64" height="64">
                                 </div>
                                 <div class="col-lg-11 col-md-6">
-                                    <p class="card-text"><strong><a href="{{$commit['author']['html_url']}}" target="_blank">{{$commit['author']['login']}}</a> - {{$commit['commit']['message']}}</strong></p>
+                                    <p class="card-text"><strong><a href="{{$commit['author']['html_url']}}" target="_blank">{{$commit['author']['login']}}</a> - {{$commit['commit']['message']}}</strong> <span class="text-muted fst-italic">{{\Carbon\Carbon::parse($commit['commit']['committer']['date'])->diffForHumans()}}</span></p>
                                     <p class="card-text ">
                                         <a href="{{$commit['html_url']}}" target="_blank"><span class="badge  bg-primary">{{$commit['sha']}}</span></a>
                                         <a href="https://github.com/DriedSponge/cs-club-website" target="_blank"><span class="badge bg-success"><i class="fas fa-code-branch"></i> master</span></a>

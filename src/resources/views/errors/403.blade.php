520ef4612ef01b760851060a7b01992a2a97eee6
DriedSponge <jordan@driedsponge.net>
2020-11-27 23:23:14 -0800

    Create error pages, might add icons to them later.

---
+++ src/resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title','403')
+@section('description','Unauthorized')
+@section('color','#FF0000')
+@section('content')
+    <div class="container text-center mt-5">
+        <h1 class="display-1 font-weight-bold">403</h1>
+        <p class="lead">You are unauthorized!</p>
+        <div class="btn-group" role="group">
+            <a href="#" class="btn btn-outline-dark" onclick="window.history.back()"><i class="fas fa-arrow-left"></i> Go Back</a>
+            <a href="{{route('home')}}" class="btn btn-outline-dark"><i class="fas fa-home"></i> Go Home</a>
+            <a target="_blank" href="https://github.com/DriedSponge/cs-club-website/issues/new"
+               class="btn btn-outline-danger"><i class="fas fa-flag"></i> Report Issue</a>
+        </div>
+    </div>s
+@endsection

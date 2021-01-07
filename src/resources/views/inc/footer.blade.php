2af20ab5a6f6df062a5ecaef786bb7bb1f90e726
DriedSponge <jordan@driedsponge.net>
2021-01-06 20:37:00 -0800

    Removed links to repo because it is now private

--- src/resources/views/inc/footer.blade.php
+++ src/resources/views/inc/footer.blade.php
@@ -10,11 +10,7 @@
             <div class="col-md-6 col-sm-12 my-2">
                 <div class="text-md-right text-center">
                     <div class="btn-group" role="group" aria-label="Basic example">
-                        <a target="_blank" href="https://github.com/DriedSponge/cs-club-website"
-                           class="btn btn-outline-dark"><i class="fab fa-github"></i> Source Code</a>
                         <a href="{{route('privacy')}}" class="btn btn-outline-dark"><i class="fas fa-lock"></i> Privacy Policy</a>
-                        <a target="_blank" href="https://github.com/DriedSponge/cs-club-website/issues/new"
-                           class="btn btn-outline-danger"><i class="fas fa-flag"></i> Report Issue</a>
                     </div>
                 </div>
             </div>

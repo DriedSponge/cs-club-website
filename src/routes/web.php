79d35f22609b49b4480c6acd762321632daea28e
DriedSponge <jordan@driedsponge.net>
2021-01-27 06:58:53 -0800

    Created controller for members page

--- src/routes/web.php
+++ src/routes/web.php
@@ -30,9 +30,7 @@ Route::get('/contact-us', function () {
 })->name("contactus");
 Route::post('/contact-us/send', 'App\Http\Controllers\ContactController@send')->name('contact-post');
 
-Route::get('/members', function () {
-    return view("pages.members");
-})->name("members");
+Route::get('/members', [\App\Http\Controllers\MembersController::class, 'index'])->name("members");
 
 Route::get('/privacy-policy', function () {
     return view("pages.privacy");

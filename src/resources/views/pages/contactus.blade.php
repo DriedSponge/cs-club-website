b9bd7ba59e4e3056547b7a581acef4629a8f96b4
DriedSponge <jordan@driedsponge.net>
2022-02-07 16:38:35 -0800

    Removed discord link

--- src/resources/views/pages/contactus.blade.php
+++ src/resources/views/pages/contactus.blade.php
@@ -15,7 +15,7 @@
                         href="https://forms.gle/KaEBXfWH2hKPJ52x6" target="_blank">google form</a></strong></p>
         <p><strong>For other questions or inquries, you can <a href="mailto:bothellcompsciclub@gmail.com"
                                                                target="_blank">email
-                    us</a>, join our <a href="https://discord.gg/uGW8uXVBJW" target="_blank">Discord Server</a>, or
+                    us</a>, or
                 fillout the below contact form.</strong></p>
     </div>
     <br>
